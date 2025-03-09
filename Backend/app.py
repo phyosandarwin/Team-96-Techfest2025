da1875aabda8e39af525639883e476165599ff51
Yu Boxuan <boxuanyu@gmail.com>
2025-03-09 19:27:35 +0800

    Added POST API endpoint

--- Backend/app.py
+++ Backend/app.py
@@ -1,17 +1,21 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 from flask_cors import CORS
+from PIL import Image
 
-# Import the fieles 
+# Import the files
 from webscraping import scrape_website
 from MachineLearning.example import extract_keywords
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/scrape', methods=['GET'])
 def scrape_endpoint():
-    # Retrieve the 'url' parameter from the query string, e.g., /scrape?url=https://example.com
+    # Retrieve the 'url' parameter from the query string
     input_url = request.args.get('url')
     
     # If no URL provided, return 400 Bad Request
@@ -28,17 +32,52 @@ def scrape_endpoint():
         for text in texts:
             output_string += f" - {text}\n"
 
+        # do text preprocessing. "output_string" is the input 
+        # Example: keywords = extract_keywords(output_string)
 
-        # Add the Machine learning function here 
-        # Here is an example, where we just extracted the keywords from the output string 
-        # TODO: Add the ML functions to call 
-        keywords = extract_keywords(output_string)
-        
-        return jsonify({"content": keywords})
+        # fact checking function needs to be called here (ie. news function)
+
+    
+        # return the matched sources , and if got classification label, return the label also, otherwise remove label 
+        return jsonify({ 
+        'contents': 'true', 
+        'matched_sources': ['source_1', 'source_2']
+        }), 200    
     
     except requests.exceptions.RequestException as e:
         # Handle any request-related exceptions
         return jsonify({"error": str(e)}), 500
+
+@app.route('/image', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(file_path)
+
+    # Open the image for processing
+    image = Image.open(file_path)
+
+    # call a CV function to extract text 
+
+
+    # do text preprocessing 
+
+
+    # fact checking function needs to be called here (ie. news function)
+
+    
+    # return the matched sources , and if got classification label, return the label also, otherwise remove label 
+    return jsonify({ 
+        'label': 'True', 
+        'matched_sources': ['source_1', 'source_2']
+    }), 200    
 
 if __name__ == "__main__":
     # Start the Flask development server

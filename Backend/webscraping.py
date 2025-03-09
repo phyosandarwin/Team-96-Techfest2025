cc4b07a7a823993d2d5a437c34751080d6d0f7b7
phyo swin <yophyoholland@gmail.com>
2025-03-09 19:37:12 +0800

    replaced webscraping file

--- Backend/webscraping.py
+++ Backend/webscraping.py
@@ -1,29 +1,18 @@
+from newspaper import Article
 import requests
-from bs4 import BeautifulSoup
 
 def scrape_website(url):
-    """
-    Takes a URL, sends a GET request, parses the HTML,
-    and returns scraped data (page title and H1 tags).
-    """
-    # Send a GET request to the provided URL
-    response = requests.get(url)
-    
-    # Raise an HTTPError if an unsuccessful status code is returned
-    response.raise_for_status()
-    
-    # Parse the HTML content
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # Extract the page title
-    page_title = soup.title.string if soup.title else "No title found"
-    
-    # Extract text from all <h1> tags
-    tags = soup.find_all('main')
-    texts = [words.get_text(strip=True) for words in tags]
-    
-    return page_title, texts
+    article = Article(url)
 
-title, texts = scrape_website("https://www.straitstimes.com/asia/east-asia/south-koreas-president-yoon-freed-after-52-days-in-detention")
-print(title)
-print(texts)
+    # get basic information about article
+    article.download()
+    article.parse()
+    article_title = article.title
+
+    # breaking down article content
+    article.nlp()
+    article_summary = article.summary
+
+    return article_title, article_summary
+
+
